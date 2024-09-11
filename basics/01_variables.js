0454c6bc25354a7793f48a274c955ad8b4ab1022
abhayraj2705 <161270201+abhayraj2705@users.noreply.github.com>
2024-09-11 17:19:18 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountuntId=1234123
+let accountname="abhayraj b"
+var accountbalance="1231231"
+accountstate="Gujurat"
+
+// accountuntId=3 cannot perform the change in the constant value operation
+console.log(accountuntId);
+
+// only prefer the let not var because var contains problems with {} problem of block space and functional occurs 
+
+console.table([accountname,accountbalance,accountstate]);
