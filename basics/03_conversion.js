a2838aecee3f8703ed4d18e46c8514c091283685
abhayraj2705 <161270201+abhayraj2705@users.noreply.github.com>
2024-09-12 12:12:44 +0000

    converion of datatypes

---
+++ basics/03_conversion.js
@@ -0,0 +1,22 @@
+let score="abhayraj b"
+console.log(typeof score);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33=>33
+// 33abc=>nan
+// true=>1 false=>0
+
+let isloogedIN=1
+let booleanISlooged= Boolean(isloogedIN);
+console.log(isloogedIN);
+
+// 1=>true , 0=>false 
+
+let sumnuber=1
+let stringnumber= String(sumnuber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
