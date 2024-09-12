d97f3dedf88c8e2d758950fd5147cdbc359b6af9
abhayraj2705 <161270201+abhayraj2705@users.noreply.github.com>
2024-09-12 19:10:29 +0000

    operation1

---
+++ basics/04_operations1.js
@@ -0,0 +1,8 @@
+let x=3
+const y=x++ // y will remain constant and x will get incremented  postfix increments the values return it  before incrementing 
+
+console.log(x,y);
+let a=4
+const b=++a // prefix first b is getting incremented and then obiously a is incrementing  prefix the incremented and it return the value after incrementing 
+
+console.log(a,b);
