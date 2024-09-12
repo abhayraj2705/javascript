5650d60319c075756c03b2695b14002a9a398f7b
abhayraj2705 <161270201+abhayraj2705@users.noreply.github.com>
2024-09-12 15:26:50 +0000

    operations

---
+++ basics/04_operations.js
@@ -0,0 +1,31 @@
+let value=3
+let negvalue=-value
+
+console.log(negvalue);
+
+let str1="abhayraj"
+let str2="b"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"4");// first adding operations will done and then pile up with the string operatins 
+console.log("1"+2+4);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+++gamecounter;
+console.log(gamecounter);
+
+
+
+
+
+
+
