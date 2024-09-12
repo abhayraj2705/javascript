68f1bf8ebcf727053c825f35c3f014c63bae1c57
abhayraj2705 <161270201+abhayraj2705@users.noreply.github.com>
2024-09-12 19:10:48 +0000

    comparision

---
+++ basics/05_comparision.js
@@ -0,0 +1,20 @@
+console.log("2"==2);
+console.log("2"===2);
+
+console.log(null == 0);
+console.log(null>0);
+console.log(null>=0);//>= contains an another operation which convert an nll to an another value that why the result is true 
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
+ 
+// if datatypes are the same for checking then there will be no issue and if datatypes are not same then the result may vary 
+//=== strict check
+
+console.log("2"===2); //it strictly checks the datatype and checks 
+
+
+
+
+
