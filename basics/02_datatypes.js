35788f750e11a9a5200f66987ca375f00f48687e
abhayraj2705 <161270201+abhayraj2705@users.noreply.github.com>
2024-09-12 11:45:26 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"user strict"; //treat all js code in newer version
+// alert(3+3);
+//alert only use in the browser engine not in node js 
+console.log(3+3)
+// priotise code readability it should be high 
+console.log("hitesh")
+
+
+let name="abhayraj"
+let age=12
+let isLoggedIn=false
+
+//number => 2 to poewer 53
+//big int
+//string =>""
+//boolean= true/false
+//null= standalone value representation of the empty value 
+//undefined=> value not defined 
+//symbol =>unique
+
+// object
+console.log(typeof "histesh");
+console.log(typeof age);
+// null is the type of the object 
+
