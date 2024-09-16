6bc3176c2a032e2387daa33868012e1802b5ae25
abhayraj2705 <161270201+abhayraj2705@users.noreply.github.com>
2024-09-16 11:46:01 +0000

    revisoinofdatatypes

---
+++ basics/06_revisiondatatypes.js
@@ -0,0 +1,47 @@
+let num=3
+console.log(typeof num);
+
+let char='a'
+console.log(typeof char);
+
+let num1=23
+let num2=33
+let num3="abhayraj"
+
+console.log(num1+num2+num3);
+
+let str="shashank"
+let str1="sinha"
+let str2=str+str1
+console.log(str2);
+
+//primitive and non primitive 
+
+// javascript is dynamic type 
+// 7 stirng number boolean null undifiend symbol bigint 
+
+// reference (non primitive)
+// array objects functions 
+
+const id =Symbol('123')
+const anotherid= Symbol('123')
+
+console.log(id == anotherid);
+
+// arrays 
+const heros=["ab","sd","sa"]
+let myObj={
+    name:"abahy",
+    age:23,
+}
+
+// functions
+
+const myfunction=function(){
+    console.log("hello world ");
+    
+}
+
+console.log(typeof myObj);
+console.log(typeof myfunction);
+
